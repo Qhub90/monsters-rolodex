3ac48ee26a925f8318a1dd0b854f5e113f5204e2
Qhub <qdaman90@gmail.com>
2019-09-30 18:30:07 -0500

    Updates

---
+++ precache-manifest.c6bce4d8d050da73a97cc8551a741c01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68e3ff70ebfb526b23d4f3f75850751",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a1406b560c63dc7c82dd",
+    "url": "/monsters-rolodex/static/css/main.997f11df.chunk.css"
+  },
+  {
+    "revision": "7d0e9689fbedf7f79dd6",
+    "url": "/monsters-rolodex/static/js/2.d312d690.chunk.js"
+  },
+  {
+    "revision": "a1406b560c63dc7c82dd",
+    "url": "/monsters-rolodex/static/js/main.122704c9.chunk.js"
+  },
+  {
+    "revision": "6d86583733b22b726541",
+    "url": "/monsters-rolodex/static/js/runtime-main.b4369954.js"
+  }
+]);
